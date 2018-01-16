7f970f12a6ebeacc0fee5cfd9c2ff4e4191027ab
Jason-oo <maojianjie007@163.com>
2018-01-16 20:43:08 +0800

    分离前端路由

---
+++ src/routes.js
@@ -0,0 +1,82 @@
+import tab from '../components/container/tab.vue';
+import chat from '../components/container/chat.vue';
+import newfriends from '../components/container/newfriends.vue';
+import login from '../components/container/login.vue';
+import register from '../components/container/register.vue';
+import details from '../components/container/details.vue';
+import together from '../components/container/together.vue';
+import addfriends from '../components/container/addfriends.vue';
+import dialogue from '../components/container/dialogue.vue';
+
+
+
+import userlist from '../components/tabs/userlist.vue';
+import contacts from '../components/tabs/contacts.vue';
+import discovery from '../components/tabs/discovery.vue';
+import mine from '../components/tabs/mine.vue';
+import success from '../components/public/msg_success.vue';
+
+export default [{
+				path: "/",
+				redirect: '/login'
+			},{
+				path: "/tab",
+				name: "tab",
+				component: tab,
+				children: [{
+					path: "wechat",
+					name: "wechat",
+					component: userlist
+				}, {
+					path: "contacts",
+					name: "contacts",
+					component: contacts
+				}, {
+					path: "discovery",
+					name: "discovery",
+					component: discovery
+				}, {
+					path: "mine",
+					name: "mine",
+					component: mine
+				}]
+			}, {
+				path: "/chat",
+				name: "chat",
+				component: chat
+			}, {
+				path: "/login",
+				name: "login",
+				component: login
+			}, {
+				path: "/register",
+				name: "register",
+				component: register
+			}, {
+				path: "/success",
+				name: "success",
+				component: success
+			}, {
+				path: "/newfriends",
+				name: "newfriends",
+				component: newfriends
+			},{
+				path: "/together",
+				name: "together",
+				component: together
+			},{
+				path: "/details",
+				name: "details",
+				component: details
+			},{
+				path: "/addfriends",
+				name: "addfriends",
+				component: addfriends
+			},{
+				path: "/dialogue",
+				name: "dialogue",
+				component: dialogue
+			},{
+				path: "**",
+				redirect: '/login'
+			}]
